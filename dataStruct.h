7cf6e62b0b75a7a5162f2372e5d39fc97a0a8067
AngryPotat0 <2485126738@qq.com>
2020-04-10 01:19:22 +0800

    second commit

---
+++ dataStruct.h
@@ -0,0 +1,15 @@
+#ifndef DATA_STRUCT
+#define DATA_STRUCT
+#include"bmpStruct.h"
+#include"stack.h"
+#include"queue.h"
+#include<stdlib.h>
+#endif
+
+#ifndef ELEMENTTYPE
+#define ELEMENTTYPE
+typedef struct{
+    int x;
+    int y;
+}elementType;
+#endif
